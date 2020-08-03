fb727697fc9f472af1b1f5ab5ad769f5ec06ef89
Md Sabbir Ahmed <sabbirtum@gmail.com>
2020-08-03 16:19:56 +0200

    bcrypt added

--- login-app/backend/models/login.model.js
+++ login-app/backend/models/login.model.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
 
 const loginSchema = new Schema(
   {
@@ -11,6 +14,32 @@ const loginSchema = new Schema(
     timestamps: true,
   }
 );
+
+loginSchema.pre("save", function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const document = this;
+    bcrypt.hash(this.password, saltRounds, function (err, hashedPassword) {
+      if (err) {
+        next(err);
+      } else {
+        document.password = hashedPassword;
+        next();
+      }
+    });
+  } else {
+    next();
+  }
+});
+
+loginSchema.methods.isCorrectPassword = function (password, callback) {
+  bcrypt.compare(password, this.password, function (err, same) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(err, same);
+    }
+  });
+};
 
 const Login = mongoose.model("Login", loginSchema);
 

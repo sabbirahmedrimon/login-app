2a3081de186fdf80dd4f07811388e5ba4b35e0d9
Md Sabbir Ahmed <sabbirtum@gmail.com>
2020-08-03 17:30:46 +0200

    authentication works partially

--- login-app/backend/models/signup.model.js
+++ login-app/backend/models/signup.model.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
-const nameSchema = new Schema(
+const saltRounds = 10;
+
+const signupSchema = new Schema(
   {
     username: { type: String, required: true },
     email: { type: String, required: true },
@@ -13,6 +16,32 @@ const nameSchema = new Schema(
   }
 );
 
-const Signup = mongoose.model("Signup", nameSchema);
+signupSchema.pre("save", function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const document = this;
+    bcrypt.hash(this.password, saltRounds, function (err, hashedPassword) {
+      if (err) {
+        next(err);
+      } else {
+        document.password = hashedPassword;
+        next();
+      }
+    });
+  } else {
+    next();
+  }
+});
+
+signupSchema.methods.isCorrectPassword = function (password, callback) {
+  bcrypt.compare(password, this.password, function (err, same) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(err, same);
+    }
+  });
+};
+
+const Signup = mongoose.model("Signup", signupSchema);
 
 module.exports = Signup;
--- login-app/backend/routes/login.js
+++ login-app/backend/routes/login.js
@@ -8,8 +8,7 @@ router.route("/").get((req, res) => {
 });
 
 router.route("/add").post((req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
+  const { email, password } = req.body;
 
   const newLogin = new Login({ email, password });
 
@@ -18,5 +17,51 @@ router.route("/add").post((req, res) => {
     .then(() => res.json("User added!"))
     .catch((err) => res.status(400).json("Error: " + err));
 });
+
+router.route("/:id").get((req, res) => {
+  Login.findById(req.params.id)
+    .then((signup) => res.json(signup))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route('/authenticate', function(req, res) {
+  const { email, password } = req.body;
+  Login.findOne({ email }, function(err, newLogin) {
+    if (err) {
+      console.error(err);
+      res.status(400)
+        .json({
+        error: 'Internal error please try again'
+      });
+    } else if (!newLogin) {
+      res.status(401)
+        .json({
+        error: 'Incorrect email or password'
+      });
+    } else {
+      user.isCorrectPassword(password, function(err, same) {
+        if (err) {
+          res.status(400)
+            .json({
+            error: 'Internal error please try again'
+          });
+        } else if (!same) {
+          res.status(401)
+            .json({
+            error: 'Incorrect email or password'
+          });
+        } else {
+          // Issue token
+          const payload = { email };
+          const token = jwt.sign(payload, secret, {
+            expiresIn: '1h'
+          });
+          res.cookie('token', token, { httpOnly: true }).sendStatus(200);
+        }
+      });
+    }
+  });
+});
+
 
 module.exports = router;
